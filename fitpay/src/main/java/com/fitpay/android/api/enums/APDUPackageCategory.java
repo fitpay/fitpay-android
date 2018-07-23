42a4641ac7b1a6c9d67da91f77f9bb16d8aa45eb
Illya Kyznetsov <>
2018-07-23 17:28:10 +0300

    Added new params to APDUPackage

---
+++ fitpay/src/main/java/com/fitpay/android/api/enums/APDUPackageCategory.java
@@ -0,0 +1,22 @@
+package com.fitpay.android.api.enums;
+
+import android.support.annotation.StringDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+public final class APDUPackageCategory {
+
+    public static final String PLATFORM = "PLATFORM";
+    public static final String DSEMS = "DSEMS";
+    public static final String LS = "LS";
+    public static final String SEI_TSM = "SEI_TSM";
+    public static final String SP_TSM = "SP_TSM";
+
+    @Retention(RetentionPolicy.SOURCE)
+    @StringDef({PLATFORM, DSEMS, LS, SEI_TSM, SP_TSM })
+    public @interface Category {
+    }
+}
+
+
