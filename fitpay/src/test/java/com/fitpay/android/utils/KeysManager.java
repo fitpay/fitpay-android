4a7682ec59523eab6a8e3f1af40d3ba54d8afb9c
Vlad Yarovyi <skygo67@gmail.com>
2018-09-18 02:01:40 +0300

    merge fixes

--- fitpay/src/test/java/com/fitpay/android/utils/KeysManager.java
+++ fitpay/src/test/java/com/fitpay/android/utils/KeysManager.java
@@ -12,15 +12,21 @@ import com.fitpay.android.api.models.security.ECCKeyPair;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.interfaces.ECPrivateKey;
+import java.security.interfaces.ECPublicKey;
+import java.security.spec.ECGenParameterSpec;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import javax.crypto.KeyAgreement;
-import javax.crypto.SecretKey;
 
 import retrofit2.Call;
 import retrofit2.Response;
@@ -37,7 +43,8 @@ final public class KeysManager {
     public static final int KEY_WV = KEY_API + 1;
     public static final int KEY_FPCTRL = KEY_WV + 1;
 
-    private static final String ALGORITHM = "ECDH";
+    private static final String ALGORITHM = "EC";
+    private static final String KEY_AGREEMENT = "ECDH";
     private static final String EC_CURVE = "secp256r1";
     private static final String KEY_TYPE = "AES";
 
@@ -72,7 +79,7 @@ final public class KeysManager {
     }
 
     // Create the public and private keys
-    private ECCKeyPair createECCKeyPair() throws Exception {
+    private ECCKeyPair createECCKeyPair() {
         String keyId = "e6b62e07-c4cc-4844-88ef-7b7d1ecb4709";
         String strPrivateKey = "308193020100301306072a8648ce3d020106082a8648ce3d030107047930770201010420cb4f12b23191eae4c4fc0f23856d8a89684b020c68cc11000a0c8381d867e68ea00a06082a8648ce3d030107a1440342000406c1ff432f49b14f9318503226e42f905333d154fe884829b36ad67ab77714b4cbb3cdf278165c5c86781862bf4254f7081cef0f3730622e183b54ce31371247";
         String strPublicKey = "3059301306072a8648ce3d020106082a8648ce3d0301070342000406c1ff432f49b14f9318503226e42f905333d154fe884829b36ad67ab77714b4cbb3cdf278165c5c86781862bf4254f7081cef0f3730622e183b54ce31371247";
@@ -90,8 +97,12 @@ final public class KeysManager {
 
 
     public PrivateKey getPrivateKey(byte[] privateKey) throws Exception {
-        KeyFactory kf = KeyFactory.getInstance(ALGORITHM,
-                SecurityProvider.getInstance().getProvider());
+        KeyFactory kf = null;
+        if (SecurityProvider.getInstance().getProvider() != null) {
+            kf = KeyFactory.getInstance(ALGORITHM, SecurityProvider.getInstance().getProvider());
+        } else {
+            kf = KeyFactory.getInstance(ALGORITHM);
+        }
         PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privateKey);
         return kf.generatePrivate(keySpec);
     }
@@ -101,16 +112,20 @@ final public class KeysManager {
     }
 
     public PublicKey getPublicKey(String algorithm, byte[] publicKey) throws Exception {
-        KeyFactory kf = KeyFactory.getInstance(algorithm,
-                SecurityProvider.getInstance().getProvider());
+        KeyFactory kf = null;
+        if (SecurityProvider.getInstance().getProvider() != null) {
+            kf = KeyFactory.getInstance(algorithm, SecurityProvider.getInstance().getProvider());
+        } else {
+            kf = KeyFactory.getInstance(algorithm);
+        }
         X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKey);
         return kf.generatePublic(keySpec);
     }
 
-    public SecretKey getSecretKey(@KeyType int type) {
+    public byte[] getSecretKey(@KeyType int type) {
 
         ECCKeyPair keyPair = getPairForType(type);
-        SecretKey secretKey = keyPair.getSecretKey();
+        byte[] secretKey = keyPair.getSecretKey();
 
         if (secretKey == null) {
             secretKey = createSecretKey(keyPair.getPrivateKey(), keyPair.getServerPublicKey());
@@ -120,25 +135,23 @@ final public class KeysManager {
         return secretKey;
     }
 
-    private SecretKey createSecretKey(String privateKeyStr, String publicKeyStr) {
+    private byte[] createSecretKey(String privateKeyStr, String publicKeyStr) {
 
         try {
             PrivateKey privateKey = getPrivateKey(Hex.hexStringToBytes(privateKeyStr));
             PublicKey publicKey = getPublicKey(Hex.hexStringToBytes(publicKeyStr));
 
             KeyAgreement keyAgreement = null;
-            try {
-                keyAgreement = KeyAgreement.getInstance(ALGORITHM,
-                        SecurityProvider.getInstance().getProvider());
-            } catch (Exception e) {
-                //hack for unit tests
-                keyAgreement = KeyAgreement.getInstance(ALGORITHM);
+            if(SecurityProvider.getInstance().getProvider() != null){
+                keyAgreement = KeyAgreement.getInstance(KEY_AGREEMENT, SecurityProvider.getInstance().getProvider());
+            } else {
+                keyAgreement = KeyAgreement.getInstance(KEY_AGREEMENT);
             }
 
             keyAgreement.init(privateKey);
             keyAgreement.doPhase(publicKey, true);
 
-            return keyAgreement.generateSecret(KEY_TYPE);
+            return keyAgreement.generateSecret();
         } catch (Exception e) {
             FPLog.e(TAG, e);
             return null;
