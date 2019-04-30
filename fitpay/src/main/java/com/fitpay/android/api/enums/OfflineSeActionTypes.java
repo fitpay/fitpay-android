9d9666d281f1dcde9d3a08ce3cc856ce1c09dc33
Vlad Yarovyi <skygo67@gmail.com>
2019-04-30 19:31:33 +0300

    add ACTIVATE and DEACTIVATE action types

--- fitpay/src/main/java/com/fitpay/android/api/enums/OfflineSeActionTypes.java
+++ fitpay/src/main/java/com/fitpay/android/api/enums/OfflineSeActionTypes.java
@@ -7,9 +7,10 @@ import java.lang.annotation.RetentionPolicy;
 
 public class OfflineSeActionTypes {
     public static final String TOP_OF_WALLET = "topOfWallet";
-
+    public static final String ACTIVATE = "activate";
+    public static final String DEACTIVATE = "deactivate";
     @Retention(RetentionPolicy.SOURCE)
-    @StringDef({TOP_OF_WALLET})
+    @StringDef({TOP_OF_WALLET, ACTIVATE, DEACTIVATE})
     public @interface Type {
     }
 }
--- fitpay/src/test/java/com/fitpay/android/api/models/card/OfflineSeActionsTest.java
+++ fitpay/src/test/java/com/fitpay/android/api/models/card/OfflineSeActionsTest.java
@@ -14,32 +14,80 @@ import org.junit.runners.MethodSorters;
 
 import java.util.List;
 
-import static org.junit.Assert.assertNotNull;
-
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class OfflineSeActionsTest extends BaseTestActions {
-
-    @ClassRule
-    public static NamedResource rule = new NamedResource(OfflineSeActionsTest.class);
 
     @Test
     public void ensurePlatformConfigurationIsSetupCorrectly() {
         final String actionsString = "{\n" +
-                "\"topOfWallet\": {\n" +
-                "\"apduCommands\": [\n" +
-                "{\n" +
-                "\"sequence\": 0,\n" +
-                "\"command\": \"00A4040009A00000015143525300\",\n" +
-                "\"type\": \"SELECT\",\n" +
-                "\"description\": \"Select CRS\",\n" +
-                "\"continueOnFailure\": false\n" +
-                "}\n" +
-                "]\n" +
-                "}\n" +
-                "}";
+                "        \"topOfWallet\": {\n" +
+                "            \"apduCommands\": [\n" +
+                "                {\n" +
+                "                    \"sequence\": 0,\n" +
+                "                    \"command\": \"00A4040009A00000015143525300\",\n" +
+                "                    \"type\": \"SELECT\",\n" +
+                "                    \"description\": \"Select CRS\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"sequence\": 1,\n" +
+                "                    \"command\": \"80F002020D4F0BA0000000041010AA030002\",\n" +
+                "                    \"type\": \"SET_STATUS\",\n" +
+                "                    \"description\": \"Set Volatile Priority\"\n" +
+                "                }\n" +
+                "            ]\n" +
+                "        },\n" +
+                "        \"activate\": {\n" +
+                "            \"apduCommands\": [\n" +
+                "                {\n" +
+                "                    \"sequence\": 0,\n" +
+                "                    \"command\": \"00A4040009A00000015143525300\",\n" +
+                "                    \"type\": \"SELECT\",\n" +
+                "                    \"description\": \"Select CRS\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"sequence\": 1,\n" +
+                "                    \"command\": \"80C3010000\",\n" +
+                "                    \"type\": \"SET_PARAM\",\n" +
+                "                    \"description\": \"Deactivate Default Card\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"sequence\": 2,\n" +
+                "                    \"command\": \"80F002020D4F0BA0000000041010AA030002\",\n" +
+                "                    \"type\": \"SET_DEFAULT_CARD\",\n" +
+                "                    \"description\": \"Set Default Card\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"sequence\": 3,\n" +
+                "                    \"command\": \"80C3010100\",\n" +
+                "                    \"type\": \"SET_PARAM\",\n" +
+                "                    \"description\": \"Activate Default Card\"\n" +
+                "                }\n" +
+                "            ]\n" +
+                "        },\n" +
+                "        \"deactivate\": {\n" +
+                "            \"apduCommands\": [\n" +
+                "                {\n" +
+                "                    \"sequence\": 0,\n" +
+                "                    \"command\": \"00A4040009A00000015143525300\",\n" +
+                "                    \"type\": \"SELECT\",\n" +
+                "                    \"description\": \"Select CRS\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"sequence\": 1,\n" +
+                "                    \"command\": \"80C3010000\",\n" +
+                "                    \"type\": \"SET_PARAM\",\n" +
+                "                    \"description\": \"Deactivate Default Card\"\n" +
+                "                }\n" +
+                "            ]\n" +
+                "        }\n" +
+                "    }";
         OfflineSeActions actions = Constants.getGson().fromJson(actionsString, OfflineSeActions.class);
         Assert.assertNotNull("actions is empty", actions);
         List<ApduCommand> towCommands = actions.getCommands(OfflineSeActionTypes.TOP_OF_WALLET);
         Assert.assertNotNull("tow commands is empty", towCommands);
+        List<ApduCommand> activate = actions.getCommands(OfflineSeActionTypes.ACTIVATE);
+        Assert.assertNotNull("activate is empty", activate);
+        List<ApduCommand> deactivate = actions.getCommands(OfflineSeActionTypes.DEACTIVATE);
+        Assert.assertNotNull("deactivate is empty", deactivate);
     }
 }
