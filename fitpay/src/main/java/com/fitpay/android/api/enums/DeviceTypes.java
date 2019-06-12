da589efb862bc31eeb722f928638750b9efc78b6
Vlad Yarovyi <skygo67@gmail.com>
2019-06-12 11:59:08 +0300

    add new device type: PHONE

--- fitpay/src/main/java/com/fitpay/android/api/enums/DeviceTypes.java
+++ fitpay/src/main/java/com/fitpay/android/api/enums/DeviceTypes.java
@@ -13,13 +13,15 @@ public final class DeviceTypes {
     public static final String MOCK = "MOCK";
     public static final String SMART_STRAP = "SMART_STRAP";
     public static final String WATCH = "WATCH";
+    public static final String PHONE = "PHONE";
 
     @Retention(RetentionPolicy.SOURCE)
     @StringDef({
             ACTIVITY_TRACKER,
             MOCK,
             SMART_STRAP,
-            WATCH
+            WATCH,
+            PHONE
     })
     public @interface Type{}
 }
